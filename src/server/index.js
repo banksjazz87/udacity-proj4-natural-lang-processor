61e23e787fe5e3fc5d2a358b085d82b6a665420c
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-04-27 05:56:55 -0400

    chore: I have my express server set up.

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,28 @@
+//using dotenv to secure the api key
 const dotenv = require('dotenv');
 dotenv.config();
 
+//using express and designating a port
+const express = require('express');
+const app = express();
+const port = 3090;
+
+//Middleware for handling incoming data
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//setting up the server
+app.get('/', (req, res) => {
+    res.send('hello world');
+    console.log(`Server is running on port ${port}`);
+});
+
+app.listen(port);
+
+
+
+//This contains the url (id) and the application key (key) for the meaning cloud api.
 const textapi = {
     application_id: process.env.API_ID,
     application_key: process.env.API_KEY
 };
-
-console.log(textapi.application_id);
-console.log(textapi.application_key);
