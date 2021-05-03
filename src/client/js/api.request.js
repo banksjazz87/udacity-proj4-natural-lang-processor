97ce8d8adcfa8b553ee8c6293dbfc9fcaf14de32
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-05-02 21:49:38 -0400

    chore: still working on getting the api call to work.

--- src/client/js/api.request.js
+++ src/client/js/api.request.js
@@ -1,5 +1,5 @@
 const apiCall = async() => {
-    const response = await fetch('/callData');
+    const response = await fetch('http://localhost:3090/callData');
     try {
         const data = await response.json();
         console.log(data);
