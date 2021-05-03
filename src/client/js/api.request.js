ea073d26430ea8e24fcbb3c71122f6a47171e080
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-05-03 05:50:43 -0400

    chore: working on getting a valid response from meaning cloud.

--- src/client/js/api.request.js
+++ src/client/js/api.request.js
@@ -1,12 +1,33 @@
-const apiCall = async() => {
+const apiCall = () => console.log(document.getElementById('input_box').value);
+
+
+/*async() => {
     const response = await fetch('http://localhost:3090/callData');
     try {
         const data = await response.json();
-        console.log(data);
+
+        const formdata = new FormData();
+        formdata.append("key", data.application_key);
+        formdata.append("txt", document.getElementById('input_box').value);
+        formdata.append("lang", "en");
+
+        const requestOptions = {
+            method: 'POST',
+            body: formdata,
+            redirect: 'follow'
+        }
+
+        const mainResponse = await fetch(response.application_id, requestOptions)
+            .then(mainResponse => ({
+                status: mainResponse.status,
+                body: mainResponse.json()
+            }))
+            .then(({ status, body }) => console.log(status, body))
+
     } catch (error) {
         console.log('error', error)
     }
-}
+}*/
 
 
 
