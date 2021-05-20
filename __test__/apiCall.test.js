0d600dddc5a31a8918619fee1fe9f495f42fbe7f
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-05-19 21:55:27 -0400

    test: still working on one more test.

--- __test__/apiCall.test.js
+++ __test__/apiCall.test.js
@@ -1,14 +1,9 @@
 import { apiCall } from "../src/client/js/api.request.js";
 import { enableFetchMocks } from 'jest-fetch-mock';
-import { outPut } from "../src/client/js/post.request.js";
 enableFetchMocks();
 
 
-test("Testing the functionality of the apiCall function", () => {
-
-    const MyLibrary = {
-        outPut: outPut()
-    };
-
-    expect(apiCall()).toBeDefined();
+test("Testing the functionality of the apiCall function", async() => {
+    const value = await apiCall();
+    expect(value).toBe(true);
 })
