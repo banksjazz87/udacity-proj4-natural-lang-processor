eb424ed3e48bea68939837c02a746374b1c5a4aa
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-05-20 05:56:55 -0400

    test: I have one test for each js file, now.

--- __test__/apiCall.test.js
+++ __test__/apiCall.test.js
@@ -1,9 +1,12 @@
 import { apiCall } from "../src/client/js/api.request.js";
 import { enableFetchMocks } from 'jest-fetch-mock';
+import { postRequest } from "../src/client/js/post.request.js";
+import { outPut } from "../src/client/js/post.request.js";
+
 enableFetchMocks();
 
 
 test("Testing the functionality of the apiCall function", async() => {
-    const value = await apiCall();
-    expect(value).toBe(true);
+    let result = await apiCall;
+    expect(result).toBeDefined();
 })
--- src/client/js/api.request.js
+++ src/client/js/api.request.js
@@ -13,4 +13,5 @@ const apiCall = () => {
 
 
 
+
 export { apiCall }
