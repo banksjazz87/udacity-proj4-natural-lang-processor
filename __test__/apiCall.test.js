c690199b09b5e5ff116310b75611169e031c1f3c
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-05-26 05:49:38 -0400

    test: all jest tests pass.

--- __test__/apiCall.test.js
+++ __test__/apiCall.test.js
@@ -6,7 +6,7 @@ import { enableFetchMocks } from 'jest-fetch-mock';
 enableFetchMocks();
 
 
-test("Testing the functionality of the apiCall function", async() => {
+test("Testing the functionality of the apiCall function, we should get an error message, because nothing is being submitted in the html input field", async() => {
     //let result = await apiCall;
-    expect(apiCall).toBeDefined();
+    expect(apiCall()).toBeDefined();
 })
--- __test__/outPut.test.js
+++ __test__/outPut.test.js
@@ -1,13 +1,14 @@
 import { outPut } from "../src/client/js/post.request.js";
 
-test("Testing the output function", () => {
+test("Testing the output function, to make sure that a list item is returned if true and an alert is returned if false", () => {
     document.body.innerHTML = `
     <input id="input_box" type="text" placeholder="Insert valid URL here" onclick="return MyLibrary.inputListener()" />
     <input id="submit_button" type="button" value="Submit" onclick="return MyLibrary.apiCall()" />`
 
     const inputVal = document.getElementById('input_box');
 
-    const positiveInput = "<li></li>"
+    const positiveInput = "<li></li>";
+
     const errorMessage = alert('Please insert a correct URL address, and make sure the URL starts with "http://" or "https://"');
 
     if ((/^https?:\/\/\S{4,}$/).test(inputVal)) {
